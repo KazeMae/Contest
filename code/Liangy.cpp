aab1c77f698f39ac92fdf3475d2236c6e24916e0
KazeMae <xiaochunfeng.x@foxmail.com>
2023-07-20 22:23:00 +0800

    awa

--- code/Liangy.cpp
+++ code/Liangy.cpp
@@ -1,52 +1,24 @@
 /**
  * @Author KAZE_mae
  * @Website cloudfall.top
- * @DateTime 2023-07-19 15:58:19
+ * @DateTime
  */
 #include <bits/stdc++.h>
 using namespace std;
+
+#define endl "\n"
+//#define int long long
+
 typedef long long ll;
 typedef pair<int, int> PII;
-#define endl "\n"
 
-//#define int long long
 const int N = 1000005; // 1e6 + 5
 
+void solve() {
+
+}
 signed main () {
     std::ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-    int n;
-    cin>> n;
-    vector<int> v(n);
-    vector<bool> st(n);
-    for(int i = 0; i < n; i++) {
-        cin>> v[i];
-        // cout<< v[i] << " ";
-    }
-    int now = 0, ding = 0, ll = 0, lr = 0;
-    for(int i = 0; i < n; i ++) {
-        if(now > 0) {
-            now --;
-            if(!st[i]) {
-                st[i] = 1;
-                ll ++;
-            }
-        }
-        if(v[i] > now) {
-            now = v[i];
-        }
-    }
-    now = 0;
-    for(int i = n - 1; i >= 0; i--) {
-        if(now > 0) {
-            now --;
-            if(!st[i]) {
-                st[i] = 1;
-                lr ++;
-            }
-        }
-        if(v[i] > now) {
-            now = v[i];
-        }
-    }
-    cout<< lr + ll <<endl;
+    int t; cin>> t; while(t --)
+        solve();
 }
--- code/Liuux.cpp
+++ code/Liuux.cpp
@@ -5,60 +5,20 @@
  */
 #include <bits/stdc++.h>
 using namespace std;
+
+#define endl "\n"
+//#define int long long
+
 typedef long long ll;
 typedef pair<int, int> PII;
-#define endl "\n"
 
-#define int long long
 const int N = 1000005; // 1e6 + 5
-pair<char, int> s[N];
+
 void solve() {
-    string ss;
-    int ls;
-    cin>> ls >> ss;
-    for(int i = 0; i < ls; i++) {
-        s[i].first = ss[i], s[i].second = i;
-        // cout<< s[i].second << " " << s[i].first <<endl;
-    }
-    stack<pair<char, int>> p;
-    for(int i = 0;i < ls; i ++)
-    {
-        if(s[i].first=='(')
-            p.push(s[i]);
-        else
-            if(p.empty()) p.push(s[i]);
-            else if(s[i].first ==')'&& p.top().first =='(') p.pop();
-            else p.push(s[i]);
-    }
-    vector<int> l, r;
-    while(!p.empty()) {
-        if(p.top().first == '(') l.push_back(p.top().second);
-        else r.push_back(p.top().second);
-        p.pop();
-    }
 
-    /*for(auto i:l) {
-        cout<< i <<" ^";
-    }
-    cout<<endl;
-    for(auto i:r) {
-        cout<< i <<" *";
-    }
-    cout<<endl;*/
-
-    if(l.size() != r.size()) cout<< -1 <<endl;
-    else {
-        int ans = 0;
-        sort(l.begin(), l.end());
-        sort(r.begin(), r.end());
-        for(int i = 0; i < l.size(); i++) {
-            ans += abs(l[i] - r[l.size() - i - 1]);
-        }
-        cout<< ans <<endl;
-    }
 }
 signed main () {
-    // std::ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    std::ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
     int t; cin>> t; while(t --)
         solve();
 }
