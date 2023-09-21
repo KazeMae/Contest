6f41846e4fa01048e8f2180b04ac771198ee0cfa
KazeMae <xiaochunfeng.x@foxmail.com>
2023-09-21 17:00:58 +0800

    9/21 17:00

--- Train/ICPC_2021_Nanjin/C.cpp
+++ Train/ICPC_2021_Nanjin/C.cpp
@@ -1,133 +1,34 @@
-/**
- * @Author      KAZE_mae
- * @Website     https://cloudfall.top/
- * @Url         
- * @DateTime    
- */
-// #include <bits/stdc++.h>
-#include <algorithm>
-#include <array>
-#include <bitset>
-#include <cassert>
-#include <chrono>
-#include <cmath>
-#include <complex>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <ctime>
-#include <deque>
-#include <iomanip>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <random>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
-#include <vector>
+#include<bits/stdc++.h>
 using namespace std;
+#define max(a, b) ((a) > (b) ? (a) : (b))
 
-using ll = long long;
-using Ld = long double;
-using uint = unsigned int;
-using ull = unsigned long long;
-template <typename T>
-using pair2 = pair<T, T>;
-using PII = pair<int, int>;
-using PLI = pair<ll, int>;
-using PLL = pair<ll, ll>;
-
-mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
-ll myRand(ll B) { 
-    return (ull)rng() % B; 
-}
-
-#define endl '\n'
-#define debug(x) cout << #x << " = " << (x) << endl
-#define abs(a) ((a) >= 0 ? (a) : -(a))
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-#define mem(a, b) memset(a, b, sizeof(a))
-// #define max(a, b) ((a) > (b) ? (a) : (b))
-#define min(a, b) ((a) < (b) ? (a) : (b))
-#define rep(i, a, n) for (int i = a; i <= n; ++i)
-#define per(i, n, a) for (int i = n; i >= a; --i)
-#define pb push_back
-#define mp make_pair
-#define fi first
-#define se second
-#define lowbit(x) ((x) & (-x))
-
-const int N = 1000005; // 1e6 + 5
-const int INF = 0x3f3f3f3f;
-const long long LNF = 0x3f3f3f3f3f3f3f3f;
-const double EPS = 1e-7;
-const double PI = acos(-1.0);
-const int MOD = 998244353;
-
-// #define int long long
-
-long long qmi(long long m, long long k, long long p = 2e18) {
-    int res = 1 % p, t = m;
-    while (k) {
-        if (k&1) res = res * t % p;
-        t = t * t % p, k >>= 1;
-    }
-    return res;
-}
-inline long long gcd(long long a, long long b) {
-    return b ? gcd(b, a % b) : a;
-}
-long long exgcd(long long a, long long b, long long &x, long long &y) {  
-    if (!b) { x = 1; y = 0; return a; }  
-    int d = exgcd(b, a % b, y, x);
-    y -= (a/b) * x;  
-    return d;
-}
-
-void solve() {
-    int n, k, mx = 0, cnt = 1;
-    cin>> n >> k;
-    vector<int> a(n);
-    map<int, int> mp, sum, ct;
-    for(int i = 0; i < n; ++ i) {
-        cin>> a[i];
-        sum[a[i]] ++;
-        if(i != 0) if(a[i] == a[i - 1]) ++ cnt;
-        else {
-            mp[a[i - 1]] = max(cnt, mp[a[i - 1]]);
-            cnt = 1;
-        }
-    }
-    mp[a[n - 1]] = max(cnt, mp[a[n - 1]]);
-    if(k != 0) {
-        for(auto i : mp) {
-            mx = max(i.second + sum[i.first + k], mx);
-            mx = max(sum[i.first], mx);
-        }
-        cnt = 1;
-        for(int i = 0; i < n; ++ i) {
-            if(i != 0)if(a[i] != a[i - 1]) {
-                mx = max({mx, 
-                    (sum[a[i - 1]] - ct[a[i - 1]] + cnt) + ct[a[i - 1] - k], 
-                    ct[a[i - 1]] + (sum[a[i - 1] - k] - ct[a[i - 1] - k])});
-                // cout<< i << " " << cnt <<endl;
-                cnt = 1;
-            }else ++ cnt;
-            ct[a[i]] ++;
-        }
-        mx = max({mx, (sum[a[n - 1]] - ct[a[n - 1]] + cnt) + ct[a[n - 1] - k], ct[a[n - 1]] + (sum[a[n - 1] - k] - ct[a[n - 1] - k])});
-    }else for(auto i : mp) mx = max(sum[i.first], mx);
-    cout<< mx <<endl;
-}
-signed main() {
+signed main()
+{
     std::ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-    // int _ = 1; cin>> _; while(_ --)
-        solve();
-  return 0;
+    int n, m;
+    vector<int> v(1000005), mm1(5000015), mm2(5000015), m_max(5000015);
+    cin>> n >> m;
+    int mmax = 0;
+    for (int i = 0; i < n; ++ i)
+    {
+        cin>> v[i];
+        v[i] += 2000005;
+        ++ mm1[v[i]];
+        mmax = max(mmax, mm1[v[i]]);
+    }
+
+    if (m == 0) cout<< mmax;
+    else {
+        for (int i = n - 1; i >= 0; --i)
+        {
+            mm2[v[i]] = max(0, mm2[v[i]] - 1);
+            ++ mm2[v[i] + m];
+            m_max[v[i] + m] = max(m_max[v[i] + m], mm2[v[i] + m]);
+        }
+        for (auto x : v) {
+            mmax = max(mmax, mm1[x] + m_max[x]);
+        }
+        cout<< mmax;
+    }
+    return 0;
 }
