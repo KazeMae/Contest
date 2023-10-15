977a00d23c3a4e03771cb2928c21fd0cf65a4e6f
KazeMae <xiaochunfeng.x@foxmail.com>
2023-10-16 03:00:57 +0800

    10/15(2)

--- code/Zhenen.cpp
+++ code/Zhenen.cpp
@@ -1,41 +1,22 @@
-#include <bits/stdc++.h>
-#define int long long
+#include <iostream>
+#include <vector>
 using namespace std;
-int n,k,sum;
-int a[100005];
-bool solve(int x)
+
+int main()
 {
-    int ans = 0;
-    for(int i = 1;i<=n;i++)
-    {
-        ans+=a[i]/x;
+    int n, k;
+    cin>> n >> k;
+    vector<int> s(n);
+    for(int i = 0; i < n; ++ i) {
+        cin>> s[i];
     }
-    return ans>=k;
-}
-signed main(void)
-{
-    cin>>n>>k;
-    for(int i = 1;i<=n;i++)
-    {
-        cin>>a[i];
-        sum += a[i];
+    for(int pos = 0; pos < n; ++ pos) {
+        if(s[pos] == k) {
+            cout<< pos + 1 <<endl;
+            return 0;
+        }
     }
-    if(sum<k){
-        cout<<sum<<endl;
-        return 0;
-    }
-    int l = 1,r = 100000005;
-    int mid;
-    while(l+1!=r)
-    {
-        mid = l+r>>1;
-        if(solve(mid)) l = mid;
-        else r = mid;
-    }
-    int langfei = 0;
-    int count = 0;
-    for(int i = 1;i<=n;i++) count+=a[i];
-    langfei = count - l*k;
-    cout<<langfei;
+    cout<< -1 <<endl;
     return 0;
 }
+
