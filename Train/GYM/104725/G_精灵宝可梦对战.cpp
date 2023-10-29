55d5bc16bcdc55d9d6bce40ed3f9b928d5261712
KazeMae <xiaochunfeng.x@foxmail.com>
2023-10-30 04:16:08 +0800

    2023女生赛

---
+++ Train/GYM/104725/G_精灵宝可梦对战.cpp
@@ -0,0 +1,182 @@
+/*******************************
+| Author:  KAZE_mae
+| Website: https://cloudfall.top
+| Problem: G. 精灵宝可梦对战
+| Contest: Codeforces - 2023年中国大学生程序设计竞赛女生专场
+| URL:     https://codeforces.com/gym/104725/problem/G
+| When:    2023-10-29 16:14:37
+| 
+| Memory:  256 MB
+| Time:    1000 ms
+*******************************/
+
+/*
+* ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷
+* ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇
+* ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽
+* ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕
+* ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕
+* ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕
+* ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄
+* ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕
+* ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿
+* ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+* ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟
+* ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠
+* ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙
+* ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣
+*/
+
+// #include <bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+using namespace std;
+
+using ll = long long;
+using Ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template <typename T>
+using pair2 = pair<T, T>;
+using PII = pair<int, int>;
+using PLI = pair<ll, int>;
+using PLL = pair<ll, ll>;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll myRand(ll B){ return (ull)rng() % B; }
+
+#define endl '\n'
+#define debug(x) cout << #x << " = " << (x) << endl
+#define abs(a) ((a) >= 0 ? (a) : -(a))
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define mem(a, b) memset(a, b, sizeof(a))
+// #define max(a, b) ((a) > (b) ? (a) : (b))
+// #define min(a, b) ((a) < (b) ? (a) : (b))
+#define rep(i, a, n) for (int i = a; i <= n; ++i)
+#define per(i, n, a) for (int i = n; i >= a; --i)
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define lowbit(x) (x&(-x))
+
+const int N = 1000005; // 1e6 + 5
+const int INF = 0x3f3f3f3f;
+const long long LNF = 0x3f3f3f3f3f3f3f3f;
+const double EPS = 1e-7;
+const double PI = acos(-1.0);
+const int MOD = 998244353;
+
+// #define int long long
+
+long long qmi(long long m, long long k, long long p = 9e18) {
+    int res = 1 % p, t = m;
+    while (k) {
+        if (k&1) res = res * t % p;
+        t = t * t % p, k >>= 1;
+    }
+    return res;
+}
+inline long long gcd(long long a, long long b) {return b ? gcd(b, a % b) : a;}
+long long exgcd(long long a, long long b, long long &x, long long &y) {  
+    if (!b) { x = 1; y = 0; return a; }  
+    int d = exgcd(b, a % b, y, x);
+    y -= (a/b) * x;  
+    return d;
+}
+
+long long Sqrt(long long N) {
+    __int128 sqrtN = sqrtl(N) - 1;
+    while (sqrtN + 1 <= N / (sqrtN + 1))sqrtN++;
+    return sqrtN;
+}
+
+// #define int long long
+
+struct Zhenen{
+    int h, a, b, c, d, e, w, te = 0;
+};
+
+void solve() ;
+signed main() {
+    std::ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    // cout<< setiosflags(ios::fixed) << setprecision(10);
+    // int _ = 1; cin>> _; while(_ --)
+        solve();
+  return 0;
+}
+// #define int long long
+
+void solve() {
+    int n, m, k, cnt = 0, flag = 0;
+    cin>> n >> m >> k;
+    queue<Zhenen> a, b;
+    for(int i = 0; i < n; ++ i) {
+        Zhenen l;
+        cin>> l.h >> l.a >> l.b >> l.c >> l.d >> l.e >> l.w;
+        a.push(l);
+    }
+    for(int i = 0; i < m; ++ i) {
+        Zhenen l;
+        cin>> l.h >> l.a >> l.b >> l.c >> l.d >> l.e >> l.w;
+        b.push(l);
+    }
+    Zhenen A = a.front(), B = b.front();
+    a.pop(), b.pop();
+    while(cnt < k) {
+        if(A.te >= A.e && A.w > max(A.a - B.c, 0) && A.w > max(A.b - B.d, 0)) 
+            A.te -= A.e, B.h -= A.w, a.push(A);
+        else B.h -= max(max(A.a - B.c, 0), max(A.b - B.d, 0)), ++ A.te, a.push(A);
+        A = a.front();
+        a.pop();
+        if(B.h <= 0) {
+            if(b.empty()) {
+                flag = 1;
+                break;
+            }
+            B = b.front();
+            b.pop();
+        }
+        if(B.te >= B.e && B.w > max(B.a - A.c, 0) && B.w > max(B.b - A.d, 0)) 
+            B.te -= B.e, A.h -= B.w, b.push(B);
+        else A.h -= max(max(B.a - A.c, 0), max(B.b - A.d, 0)), ++ B.te, b.push(B);
+        B = b.front();
+        b.pop();
+        if(A.h <= 0) {
+            if(a.empty()) {
+                flag = 2;
+                break;
+            }
+            A = a.front();
+            a.pop();
+        }
+        ++ cnt;
+    }
+    if(flag == 0) cout << "Draw" <<endl;
+    else if(flag == 1) cout << "Alice" <<endl;
+    else cout << "Bob" <<endl;
+} 
---
+++ Train/GYM/104725/L_养成游戏.cpp
@@ -0,0 +1,161 @@
+/*******************************
+| Author:  KAZE_mae
+| Website: https://cloudfall.top
+| Problem: L. 养成游戏
+| Contest: Codeforces - 2023年中国大学生程序设计竞赛女生专场
+| URL:     https://codeforces.com/gym/104725/problem/L
+| When:    2023-10-29 15:04:17
+| 
+| Memory:  256 MB
+| Time:    1000 ms
+*******************************/
+
+/*
+* ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷
+* ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇
+* ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽
+* ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕
+* ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕
+* ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕
+* ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄
+* ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕
+* ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿
+* ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+* ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟
+* ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠
+* ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙
+* ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣
+*/
+
+// #include <bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+using namespace std;
+
+using ll = long long;
+using Ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template <typename T>
+using pair2 = pair<T, T>;
+using PII = pair<int, int>;
+using PLI = pair<ll, int>;
+using PLL = pair<ll, ll>;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll myRand(ll B){ return (ull)rng() % B; }
+
+#define endl '\n'
+#define debug(x) cout << #x << " = " << (x) << endl
+#define abs(a) ((a) >= 0 ? (a) : -(a))
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define mem(a, b) memset(a, b, sizeof(a))
+// #define max(a, b) ((a) > (b) ? (a) : (b))
+// #define min(a, b) ((a) < (b) ? (a) : (b))
+#define rep(i, a, n) for (int i = a; i <= n; ++i)
+#define per(i, n, a) for (int i = n; i >= a; --i)
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define lowbit(x) (x&(-x))
+
+const int N = 1000005; // 1e6 + 5
+const int INF = 0x3f3f3f3f;
+const long long LNF = 0x3f3f3f3f3f3f3f3f;
+const double EPS = 1e-7;
+const double PI = acos(-1.0);
+const int MOD = 998244353;
+
+// #define int long long
+
+long long qmi(long long m, long long k, long long p = 9e18) {
+    int res = 1 % p, t = m;
+    while (k) {
+        if (k&1) res = res * t % p;
+        t = t * t % p, k >>= 1;
+    }
+    return res;
+}
+inline long long gcd(long long a, long long b) {return b ? gcd(b, a % b) : a;}
+long long exgcd(long long a, long long b, long long &x, long long &y) {  
+    if (!b) { x = 1; y = 0; return a; }  
+    int d = exgcd(b, a % b, y, x);
+    y -= (a/b) * x;  
+    return d;
+}
+
+long long Sqrt(long long N) {
+    __int128 sqrtN = sqrtl(N) - 1;
+    while (sqrtN + 1 <= N / (sqrtN + 1))sqrtN++;
+    return sqrtN;
+}
+
+#define int long long
+
+struct Zhenen {
+    int i, j, op, a, b, d, v;
+};
+
+void ope(int k, vector<int> &A, int kk) {
+    int i = 1;
+    for(auto &l : A) l = 0;
+    while(k) {
+        A[i] += k % kk;
+        k /= kk, ++ i;
+    }
+}
+
+void solve() ;
+signed main() {
+    std::ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    // cout<< setiosflags(ios::fixed) << setprecision(10);
+    // int _ = 1; cin>> _; while(_ --)
+        solve();
+  return 0;
+}
+// #define int long long
+
+void solve() {
+    int n, m, k;
+    cin>> n >> m >> k;
+    vector<Zhenen> a(m);
+    vector<int> A(n + 1);
+    for(int i = 0; i < m; ++ i)
+        cin>> a[i].i >> a[i].j >> a[i].op >> a[i].a >> a[i].b >> a[i].d >> a[i].v;
+    int end = qmi(k + 1, n), ans = 0;
+    for(int i = 0; i < end; ++ i) {
+        ope(i, A, k + 1);
+        // for(auto k : A) cout << k << " "; cout <<endl;
+        int sum = 0;
+        for(auto y : a) 
+            if(y.op) sum += (y.a * A[y.i] + y.b * A[y.j] >= y.d ? y.v : 0);
+            else sum += (y.a * A[y.i] + y.b * A[y.j] <= y.d ? y.v : 0);
+        ans = max(sum, ans);
+    }
+    cout<< ans <<endl;
+} 
